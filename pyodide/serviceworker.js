3719567be3f1fc61c25943aab27828ff91367bbc
Chris Patuzzo <chris@patuzzo.co.uk>
2024-01-24 14:43:39 +0000

    Use a service worker to set the needed HTTP headers
    https://github.com/RaspberryPiFoundation/editor-ui/issues/903
    
    Based on: https://dev.to/stefnotch/enabling-coop-coep-without-touching-the-server-2d3n

---
+++ pyodide/serviceworker.js
@@ -0,0 +1,25 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") { return; }
+
+  event.respondWith(
+    fetch(event.request).then(response => {
+      const body = response.body;
+      const status = response.status;
+      const headers = new Headers(response.headers);
+      const statusText = response.statusText;
+
+      headers.set("Cross-Origin-Embedder-Policy", "require-corp");
+      headers.set("Cross-Origin-Opener-Policy", "same-origin");
+
+      return new Response(body, { status, statusText, headers });
+    }).catch(console.error)
+  );
+});
